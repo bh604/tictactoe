aff0006a03cef234d7b3d6fab9a070bca7bbb65b
Brian Hoang <bhoangster@live.ca>
2022-12-22 12:01:38 -0800

    added game logic, changed class name cell to box in html

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let boxes = Array.from(document.getElementsByClassName('box'))
+let xText = 'X'
+let oText = 'O'
+document.querySelector('.box').addEventListener('click', markBox)
+
+
+
+
+console.log(boxes)
