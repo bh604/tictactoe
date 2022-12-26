cd3e4f7a3a9b3cb7591967d899eba8da5a189655
Brian Hoang <bhoangster@live.ca>
2022-12-26 10:25:23 -0800

    worked on game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 let boxes = Array.from(document.getElementsByClassName('box'))
 let xText = 'X'
 let oText = 'O'
-document.querySelector('.box').addEventListener('click', markBox)
+let currentPlayer = xText
+let spaces = Array(9).fill(null)
 
 
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
+}
 
 
-console.log(boxes)
+function boxClicked(e) {
+    const id = e.target.id
+
+    if(!spaces[id]) {
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        currentPlayer = currentPlayer == xText ? oText : xText
+    }
+}
+
+startGame()
