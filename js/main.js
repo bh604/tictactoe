2a869c3c8aef9d5790aa51dfb580b56b58af533f
Brian Hoang <bhoangster@live.ca>
2023-01-16 13:28:17 -0800

    made some fixes

--- js/main.js
+++ js/main.js
@@ -1,37 +1,57 @@
 let boxes = Array.from(document.getElementsByClassName('box'))
-let xText = 'X'
-let oText = 'O'
+
+
+const xText = 'X'
+const oText = 'O'
 let currentPlayer = xText
 let spaces = Array(9).fill(null)
 
 
-
-function startGame() {
+const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 
-function boxClicked(click) {
-    console.log(click.target)
-    // check if box clicked is null, if null change it to currentPlayer, then change currentPlayer to next
-    if(boxes[id] == null) {
+function boxClicked(e){
+     const id = e.target.id
+        // check if box is null, fill box with currentplayer
+     if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+        // check if player has won
+        if(playerHasWon() !==false){
+            console.log(`${currentPlayer} has won!`)
+            let winning_blocks = playerHasWon()
 
-    }
+            winning_blocks 
+        }
+        // change currentplayer to next
+        currentPlayer = currentPlayer == xText ? oText : xText
+     }
 }
 
-let winningCombos = [[]]
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
+function playerHasWon(){
+    for(const condition of winningCombos) {
+        let [a,b,c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])){
+            return [a,b,c]
+        }
+    }
+    return false
+}
 
 startGame()
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -63,7 +83,7 @@ startGame()
 
 // function boxClicked(e) {
 //     const id = e.target.id
-
+    
 //     if(!spaces[id]) {
 //         spaces[id] = currentPlayer
 //         e.target.innerText = currentPlayer
